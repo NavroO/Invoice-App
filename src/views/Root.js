261e8b2c0c40bc6ebfafb7c85a5e47849824bbbb
Navro <tomeknawrocki2000@gmail.com>
2022-01-04 18:18:24 +0100

    Added basic table with fake data

--- src/views/Root.js
+++ src/views/Root.js
@@ -1,10 +1,60 @@
 import React from 'react';
+import Container from '@mui/material/Container';
+import DeleteIcon from '@mui/icons-material/Delete';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+function createUser(id, name, email, role) {
+  return { id, name, email, role };
+}
+
+const rows = [
+  createUser(1, "Tomek", "tomeknawrocki2000@gmail.com", "Admin"),
+  createUser(2, "Martyna", "test@test.com", "User"),
+  createUser(1, "Tomek", "tomeknawrocki2000@gmail.com", "Admin"),
+  createUser(1, "Tomek", "tomeknawrocki2000@gmail.com", "Admin"),
+  createUser(1, "Tomek", "tomeknawrocki2000@gmail.com", "Admin"),
+];
 
 const Root = () => {
   return (
-    <h1>
-      Hello world
-    </h1>
+    <Container fixed>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>ID</TableCell>
+              <TableCell align="right">Name</TableCell>
+              <TableCell align="right">Email</TableCell>
+              <TableCell align="right">Role</TableCell>
+              <TableCell align="right">Actions</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow
+                key={row.id}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {row.id}
+                </TableCell>
+                <TableCell align="right">{row.name}</TableCell>
+                <TableCell align="right">{row.email}</TableCell>
+                <TableCell align="right">{row.role}</TableCell>
+                <TableCell align="right">
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Container>
   );
 }
 
